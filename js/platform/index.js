edf440e3014b915cd8236079955d68adca5bbd28
edoedac0 <edinvelic2011@gmail.com>
2026-06-01 17:10:29 +0200

    Prepare big-update for main merge

--- js/platform/index.js
+++ js/platform/index.js
@@ -56,7 +56,7 @@ function detectPlatformName() {
     return override;
   }
   const searchParams = String(globalThis.location?.search || "").toLowerCase();
-  if (searchParams.includes("wrapper=tizen")) {
+  if (searchParams.includes("wrapper=tizen") || searchParams.includes("source=tizenbrew")) {
     return "tizen";
   }
   const userAgent = String(globalThis.navigator?.userAgent || "").toLowerCase();
